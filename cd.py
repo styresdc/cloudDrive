a6745d2917211ad7bcacb0dc7e8084521a8b37e7
Dixon <styresd@gmail.com>
2018-05-06 11:52:15 -0500

    update

--- cd.py
+++ cd.py
@@ -7,6 +7,7 @@ import box
 import os
 import traceback
 from multiprocessing import Process
+import time
 
 filedict = {}
 num_files = 0
@@ -14,95 +15,149 @@ dbUp = db.TransferData()
 gdUp = gd.TransferData()
 boxUp = box.TransferData()
 
-#read in a file and split/upload it to enable aggregation. only text files for now
-#Splits a file on "Spliting Text"
+"""read in a file and split/upload it to enable aggregation. only text files for now
+Splits a file on "Spliting Text"
+backup ability if selected
+"""
 class FileSplit:
-    def __init__(self, file_path):
+    def __init__(self, file_path, back):
         self.file_path = file_path
         self.file_count = 0
-        self.service = 0
+        self.back = back
     def split(self):
-        files = open(self.file_path,'r').read().split('Splitting Text')
-        names = [self.file_path + str(num) for num in range(len(files))]
-        for num,file in enumerate(files):
-            open(names[num],'w').write(file)
-            self.file_count += 1
+        """split a file on "Splitting text" and make duplicates if redundant mode
+        """
+        if(self.back == 'y'):
+            files = open(self.file_path,'r').read().split('Splitting Text')
+            names = [self.file_path + str(num) for num in range(len(files))]
+            for num,file in enumerate(files):
+                open(names[num],'w').write(file)
+                self.file_count += 1
+            backNames = [self.file_path + str(num) + 'b' for num in range(len(files))]
+            for num,file in enumerate(files):
+                open(backNames[num],'w').write(file)
+        else:
+            files = open(self.file_path,'r').read().split('Splitting Text')
+            names = [self.file_path + str(num) for num in range(len(files))]
+            for num,file in enumerate(files):
+                open(names[num],'w').write(file)
+                self.file_count += 1
     def ledgerWrite(self):
-            open("ledger.txt",'a').write(str(num_files) + ' ' + str(self.file_count) + ' ' + self.file_path + '\n')
+            """append to the ledger when adding files, upload as well
+            """
+            if(self.back == 'y'):
+                open("ledger.txt",'a').write(str(num_files) + ' ' + str(self.file_count) + ' ' + self.file_path + ' ' + "y" + '\n')
+            else:
+                open("ledger.txt",'a').write(str(num_files) + ' ' + str(self.file_count) + ' ' + self.file_path + ' ' + "n" + '\n')
             dbUp.upload_file("ledger.txt", '/ledger.txt')
     def uploader(self):
         j = 0
+        a = []
+        b = []
         for x in range(0,self.file_count):
             if(j == 0):
-                t1 = Process(target = dbUp.upload_file, args = (self.file_path + str(x), '/' + self.file_path + str(x)),)
-                #dbUp.upload_file(self.file_path + str(x), '/' + self.file_path + str(x))
-                t1.start()
+                a.append(Process(target = dbUp.upload_file, args = (self.file_path + str(x), '/' + self.file_path + str(x)),))
+                a[x].start()
                 j+=1
             elif(j == 1):
-                t2 = Process(target = gdUp.upload_file, args = (self.file_path + str(x),))
-                #gdUp.upload_file(self.file_path + str(x))
-                t2.start()
+                a.append(Process(target = gdUp.upload_file, args = (self.file_path + str(x),)))
+                a[x].start()
                 j+=1
             elif(j == 2):
-                t3 = Process(target = boxUp.upload_file, args = (self.file_path + str(x),))
-                #boxUp.upload_file(self.file_path + str(x))
-                t3.start()
+                a.append(Process(target = boxUp.upload_file, args = (self.file_path + str(x),)))
+                a[x].start()
                 j = 0
-        t1.join()
-        t2.join()
-        t3.join()
+        if(self.back == 'y'):
+            j = 0
+            for x in range(0,self.file_count):
+                if(j == 0):
+                    b.append(Process(target = dbUp.upload_file, args = (self.file_path + str(x)+'b', '/'+self.file_path+str(x)+'b'),))
+                    b[x].start()
+                    j+=1
+                elif(j == 1):
+                    b.append(Process(target = gdUp.upload_file, args = (self.file_path + str(x)+'b',)))
+                    b[x].start()
+                    j+=1
+                elif(j == 2):
+                    b.append(Process(target = boxUp.upload_file, args = (self.file_path + str(x)+'b',)))
+                    b[x].start()
+                    j = 0
         global num_files
         num_files += 1
 #rebuild file parts
 class FileMake:
-    def __init__(self, file_name, numbers):
+    def __init__(self, file_name, numbers, back):
         self.file_name = file_name
         self.numbers = int(numbers)
+        self.back = back
+        self.error = "no"
     def build(self):
         with open(self.file_name + '_cloud.txt', 'w') as outfile:
             for x in range (0, self.numbers):
-                    with open(self.file_name + str(x)) as infile:
-                        outfile.write(infile.read())
-                        os.remove(self.file_name + str(x))
+                    if(self.error == "yes"):
+                        with open(self.file_name + str(x) + 'b') as infile:
+                            outfile.write(infile.read())
+                            os.remove(self.file_name + str(x) + 'b')
+                    else:
+                        with open(self.file_name + str(x)) as infile:
+                            outfile.write(infile.read())
+                            os.remove(self.file_name + str(x))
     def download(self):
         j=0
+        a = []
         for x in range(0, self.numbers):
             if(j == 0):
-                t1 = Process(target = dbUp.download_file, args = (self.file_name + str(x),))
-                #dbUp.download_file(self.file_name + str(x))
-                t1.start()
+                a.append(Process(target = dbUp.download_file, args = (self.file_name + str(x),)))
+                a[x].start()
                 j+=1
             elif(j == 1):
-                t2 = Process(target = gdUp.download_file, args = (self.file_name + str(x),))
-                #gdUp.download_file(self.file_name + str(x))
-                t2.start()
+                a.append(Process(target = gdUp.download_file, args = (self.file_name + str(x),)))
+                a[x].start()
                 j+=1
             elif(j == 2):
-                t3 = Process(target = boxUp.download_file, args = (self.file_name + str(x),))
-                #boxUp.download_file(self.file_name + str(x))
-                t3.start()
+                a.append(Process(target = boxUp.download_file, args = (self.file_name + str(x),)))
+                a[x].start()
                 j=0
-        t1.join()
-        t2.join()
-        t3.join()
+        for job in a:
+            job.join()
+        ##redundant backups
+        for x in range(0, self.numbers):
+            if(!(os.path.isfile(self.file_name + str(x))):
+                self.error = "yes"
+                j=0
+                a = []
+                for x in range(0, self.numbers):
+                    if(j == 0):
+                        a.append(Process(target = dbUp.download_file, args = (self.file_name + str(x) + 'b',)))
+                        a[x].start()
+                        j+=1
+                    elif(j == 1):
+                        a.append(Process(target = gdUp.download_file, args = (self.file_name + str(x) + 'b',)))
+                        a[x].start()
+                        j+=1
+                    elif(j == 2):
+                        a.append(Process(target = boxUp.download_file, args = (self.file_name + str(x)+ 'b',)))
+                        a[x].start()
+                        j=0
+                for job in a:
+                    job.join()
+                return
     def delete(self):
         j=0
+        a = []
         for x in range(0, self.numbers):
             if(j == 0):
-                t1 = Process(target = dbUp.delete_file, args = (self.file_name + str(x),))
-                t1.start()
+                a.append(Process(target = dbUp.delete_file, args = (self.file_name + str(x),)))
+                a[x].start()
                 j+=1
             elif(j == 1):
-                t2 = Process(target = gdUp.delete_file, args = (self.file_name + str(x),))
-                t2.start()
+                a.append(Process(target = gdUp.delete_file, args = (self.file_name + str(x),)))
+                a[x].start()
                 j+=1
             elif(j == 2):
-                t3 = Process(target = boxUp.delete_file, args = (self.file_name + str(x),))
-                t3.start()
+                a.append(Process(target = boxUp.delete_file, args = (self.file_name + str(x),)))
+                a[x].start()
                 j=0
-        t1.join()
-        t2.join()
-        t3.join()
 #read ledger, create dict
 class ledgerRead:
     def __init__(self):
@@ -122,12 +177,16 @@ def main():
             dbUp.download_file("ledger.txt")
             ledgerRead()
         except:
-            print("Unable to find leger, starting new")
+            print("Unable to find ledger, starting new")
     #print welcome @ selection
     #list file or upload file
     print("Welcome to cloudDrive....")
     while(True):
-        print("'1' to upload , '2' to list files , '3' to download, '4' to delete, '5' to quit")
+        print("'1' to upload")
+        print("'2' to list files")
+        print("'3' to download")
+        print("'4' to delete")
+        print("'5' to quit")
         text = raw_input("Make a Selection ")
         if(text == '5'):
             print("Goodbye")
@@ -140,7 +199,7 @@ def main():
                 if(num_files == 0):
                     print("No Files in ledger")
                 else:
-                    print("Index, Number of parts, File Name")
+                    print("Index, Number of parts, File Name, Redundant")
                     print(filedict)
             except:
                 print("Error reading ledger file")
@@ -170,16 +229,15 @@ def main():
             _=os.system("clear")
             try:
                 ledgerRead()
-                print("Index, Number of parts, File Name")
+                print("Index, Number of parts, File Name, Redundant")
                 print(filedict)
+                file_name = raw_input("Give Index for download: ")
+                file_name = filedict[int(file_name)].split(',')
+                fileBuild = FileMake(file_name[1], file_name[0], file_name[2])
+                fileBuild.download()
+                fileBuild.build()
             except:
                 print("Error reading ledger file")
-                traceback.print_exc()
-            file_name = raw_input("Give Index for del: ")
-            file_name = filedict[int(file_name)].split(',')
-            fileBuild = FileMake(file_name[1], file_name[0])
-            fileBuild.download()
-            fileBuild.build()
         elif(text == '2'):
             _=os.system("clear")
             try:
@@ -192,21 +250,24 @@ def main():
                         print(filedict[x])
             except:
                 print("Error reading ledger file")
-                traceback.print_exc()
         elif(text == '1'):
             file_path = raw_input("Give Text filepath for upload: ")
-            fileSplitter = FileSplit(file_path)
-            try:
-                fileSplitter.split()
-                fileSplitter.ledgerWrite()
-                fileSplitter.uploader()
-                dele = raw_input("Upload complete, Delete local file? y/n ")
-                if(dele == "y"):
-                    os.remove(self.file_path + str(x))
-                else:
-                    print("File not deleted")
-            except:
-                print("Error Reading Source File")
-                traceback.print_exc()
+            back = raw_input("Make redundant backups? (y/n): ")
+            if(back != 'y' and back != 'n'):
+                print("please select 'y' or 'n'")
+            else:
+                fileSplitter = FileSplit(file_path,back)
+                try:
+                    fileSplitter.split()
+                    fileSplitter.ledgerWrite()
+                    fileSplitter.uploader()
+                    dele = raw_input("Upload complete, Delete local file? y/n ")
+                    if(dele == "y"):
+                        os.remove(self.file_path + str(x))
+                    else:
+                        print("File not deleted")
+                except:
+                    print("Error Reading Source File")
+                    traceback.print_exc()
 if __name__ == '__main__':
     main()
--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 import dropbox
 import os
-
+import time
 class TransferData:
     def __init__(self):
         try:
@@ -12,16 +12,18 @@ class TransferData:
     def upload_file(self, file_from, file_to):
         """upload a file to Dropbox
         """
-
-        with open(file_from, 'rb') as f:
-            self.dbx.files_upload(f.read(), file_to,mode=dropbox.files.WriteMode('overwrite', None))
-        if(file_from == "ledger.txt"):
-            return
+        try:
+            with open(file_from, 'rb') as f:
+                self.dbx.files_upload(f.read(), file_to,mode=dropbox.files.WriteMode('overwrite', None))
+            if(file_from == "ledger.txt"):
+                return
+        except:
+            pass
         os.remove(file_from)
     def download_file(self, file_from):
         """download file from Dropbox
         """
-        self.dbx.files_download_to_file(file_from, '/' + file_from)
+        self.dbx.files_download_to_file(file_from, "/" + file_from)
     def delete_file(self, file_from):
         """delete file from Dropbox
         """
